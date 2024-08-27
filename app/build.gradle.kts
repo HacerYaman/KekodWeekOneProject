6de6df364187277190d500f5b478af41bc48553e
Hacer <yaman.hacer@outlook.com>
2024-08-27 15:25:47 +0300

    gitignore add, third party dependencies add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.navigationSafeArgs)
+    alias(libs.plugins.com.google.dagger.hilt.android)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -27,12 +33,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 dependencies {
@@ -45,4 +55,50 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    // Test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+
+    // ViewModel
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.activity.ktx)
+
+    // LiveData
+    implementation(libs.lifecycle.livedata)
+
+    // Picasso
+    implementation(libs.picasso)
+
+    // RecyclerView for Search
+    implementation(libs.recyclerview)
+
+    // Lottie
+    implementation(libs.lottie)
+
+    // Animated Svg for Splash
+    implementation(libs.animated.svg.view)
+
+    // Room
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Detekt
+    detektPlugins(libs.detekt)
+}
+
+detekt {
+    config.setFrom(file("$rootDir/detekt/detektConfig.yml"))
+    source.from(files("src/main/kotlin", "src/test/kotlin"))
+    parallel = true
+    autoCorrect = true
+    buildUponDefaultConfig = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.navigationSafeArgs) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) version "2.51" apply false
+    alias(libs.plugins.detekt) apply false
 }
