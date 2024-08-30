59af260f368fb03d9b510eb3feedc4dfc90fb876
Hacer <yaman.hacer@outlook.com>
2024-08-30 23:36:33 +0300

    back icon function fix

--- app/src/main/java/com/example/kekodweekoneproject/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/kekodweekoneproject/ui/favorites/FavoritesFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kekodweekoneproject.R
@@ -21,12 +23,17 @@ class FavoritesFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_favorites, container, false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        val backButton = view.findViewById<ImageView>(R.id.imageView2)
         adapter = FavoritesAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
         viewModel.favoriteQuotes.observe(viewLifecycleOwner) { quotes ->
             adapter.submitList(quotes)
+        }
+
+        backButton.setOnClickListener {
+            findNavController().navigateUp()
         }
 
         return view
